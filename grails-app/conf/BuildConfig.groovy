da247f5e8fef927ba3ce7ebeae0e06ecec8a5898
Iván López <lopez.ivan@gmail.com>
2013-07-07 23:20:26 +0200

    Initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,36 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    legacyResolve true // whether to do a secondary resolve on plugin installation, not advised but set here for backwards compatibility
+    repositories {
+        grailsCentral()
+        mavenCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+        runtime 'postgresql:postgresql:9.1-901.jdbc4'
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.2.0",
+              ":rest-client-builder:1.0.3") {
+            export = false
+        }
+    }
+}
