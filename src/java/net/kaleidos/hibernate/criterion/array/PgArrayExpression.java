21f0006f533c800fc58b6a51f57670a493e9be0e
Matt Feury <mattfeury@gmail.com>
2013-08-23 19:52:19 -0400

    Properly convert IdentityEnumArrayType values for CriteriaQueries

--- src/java/net/kaleidos/hibernate/criterion/array/PgArrayExpression.java
+++ src/java/net/kaleidos/hibernate/criterion/array/PgArrayExpression.java
@@ -8,6 +8,9 @@ import org.hibernate.engine.TypedValue;
 import org.hibernate.type.Type;
 import org.hibernate.util.StringHelper;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 /**
  * Constrains a property in an array
  */
@@ -42,6 +45,19 @@ public class PgArrayExpression implements Criterion {
         Object[] arrValue;
         if ("net.kaleidos.hibernate.usertype.IntegerArrayType".equals(propertyTypeName)) {
             arrValue = pgCriteriaUtils.getValueAsArrayOfType(value, Integer.class);
+        } else if ("net.kaleidos.hibernate.usertype.IdentityEnumArrayType".equals(propertyTypeName)) {
+            if (value instanceof List) {
+                List valueAsList = (List)value;
+                arrValue = (Object[]) Array.newInstance(Integer.class, valueAsList.size());
+
+                for(int i=0; i < valueAsList.size(); i++) {
+                    arrValue[i] = ((Enum)(valueAsList.get(i))).ordinal();
+                }
+            } else {
+                Object converted = ((Enum)value).ordinal();
+                arrValue = pgCriteriaUtils.getValueAsArrayOfType(converted, Integer.class);
+            }
+
         } else if ("net.kaleidos.hibernate.usertype.LongArrayType".equals(propertyTypeName)) {
             arrValue = pgCriteriaUtils.getValueAsArrayOfType(value, Long.class);
         } else if ("net.kaleidos.hibernate.usertype.StringArrayType".equals(propertyTypeName)) {
