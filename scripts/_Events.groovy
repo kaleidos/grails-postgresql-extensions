fc78fb8ef0c926a69b717b7568828594338f63a8
Iván López <lopez.ivan@gmail.com>
2013-10-28 13:19:50 +0100

    Add hook to compile AST before project

---
+++ scripts/_Events.groovy
@@ -0,0 +1,20 @@
+eventCompileStart = { target ->
+    compileAST(basedir, classesDirPath)
+}
+
+def compileAST(def srcBaseDir, def destDir) {
+    ant.sequential {
+        println "Precompiling AST Transformations ..."
+        println "src ${srcBaseDir} ${destDir}"
+        path id: "grails.compile.classpath", compileClasspath
+        def classpathId = "grails.compile.classpath"
+        mkdir dir: destDir
+        groovyc(destdir: destDir,
+            srcDir: "$srcBaseDir/src/groovy/net/kaleidos/hibernate/postgresql/hstore",
+            classpathref: classpathId,
+            verbose: grailsSettings.verboseCompile,
+            stacktrace: "yes",
+            encoding: "UTF-8")
+        println "Done precompiling AST Transformations!"
+    }
+}
