c19500769cb75f1abed0fb2ccd6ceaaef3c582c2
Iván López <lopez.ivan@gmail.com>
2014-01-19 15:13:07 +0100

    Set work dir as target/work

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,4 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
