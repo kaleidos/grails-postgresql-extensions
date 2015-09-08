1da08d0047dd2e56520a45651f3b4f9bd3712f17
Iván López <lopez.ivan@gmail.com>
2015-09-08 09:28:13 +0200

    Small cleanup

--- src/java/net/kaleidos/hibernate/order/OrderByRandom.java
+++ src/java/net/kaleidos/hibernate/order/OrderByRandom.java
@@ -5,20 +5,10 @@ import org.hibernate.HibernateException;
 import org.hibernate.criterion.CriteriaQuery;
 import org.hibernate.criterion.Order;
 
-/**
- * Extends {@link org.hibernate.criterion.Order} to allow ordering by an SQL formula passed by the user.
- * Is simply appends the <code>sqlFormula</code> passed by the user to the resulting SQL query, without any verification.
- * @author Sorin Postelnicu
- * @since Jun 10, 2008
- */
 public class OrderByRandom extends Order {
 
     private static final String ORDER_RANDOM = "random()";
 
-    /**
-     * Constructor for Order.
-     * @param sqlFormula an SQL formula that will be appended to the resulting SQL query
-     */
     protected OrderByRandom() {
         super(ORDER_RANDOM, true);
     }
@@ -31,12 +21,6 @@ public class OrderByRandom extends Order {
         return ORDER_RANDOM;
     }
 
-    /**
-     * Custom order
-     *
-     * @param sqlFormula an SQL formula that will be appended to the resulting SQL query
-     * @return Order
-     */
     public static Order byRandom() {
         return new OrderByRandom();
     }
