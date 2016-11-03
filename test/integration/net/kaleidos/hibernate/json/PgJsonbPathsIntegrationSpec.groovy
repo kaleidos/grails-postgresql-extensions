c3a2fd06338c92603c750557d3a8fb043dcb2993
Iván López <lopez.ivan@gmail.com>
2016-11-03 12:40:31 +0100

    Fix indentation of tests

--- test/integration/net/kaleidos/hibernate/json/PgJsonbPathsIntegrationSpec.groovy
+++ test/integration/net/kaleidos/hibernate/json/PgJsonbPathsIntegrationSpec.groovy
@@ -11,12 +11,12 @@ class PgJsonbPathsIntegrationSpec extends Specification {
     @Unroll
     void 'Test equals finding nested values (jsonb)'() {
         setup:
-        new TestMapJsonb(data: [name: 'Iván', lastName: 'López', nested: [a: 1, b: 2]]).save(flush: true)
-        new TestMapJsonb(data: [name: 'Alonso', lastName: 'Torres', nested: [a: 2, b: 3]]).save(flush: true)
-        new TestMapJsonb(data: [name: 'Iván', lastName: 'Pérez', nested: [a: 1, b: 5]]).save(flush: true)
+            new TestMapJsonb(data: [name: 'Iván', lastName: 'López', nested: [a: 1, b: 2]]).save(flush: true)
+            new TestMapJsonb(data: [name: 'Alonso', lastName: 'Torres', nested: [a: 2, b: 3]]).save(flush: true)
+            new TestMapJsonb(data: [name: 'Iván', lastName: 'Pérez', nested: [a: 1, b: 5]]).save(flush: true)
 
         when:
-        def result = pgJsonbTestSearchService.search('pgJson', 'data', '#>>', '{nested, a}', '=', value)
+            def result = pgJsonbTestSearchService.search('pgJson', 'data', '#>>', '{nested, a}', '=', value)
 
         then:
             result.size() == size
@@ -24,20 +24,20 @@ class PgJsonbPathsIntegrationSpec extends Specification {
 
         where:
             value || size
-                1 || 2 // there are 2 items with nested.a equal to 1
-                2 || 1
-                3 || 0
+            1     || 2 // there are 2 items with nested.a equal to 1
+            2     || 1
+            3     || 0
     }
 
     @Unroll
-    void 'Test equals finding nested values (jsonb)'() {
+    void 'Test greater than finding nested values (jsonb)'() {
         setup:
-        new TestMapJsonb(data: [name: 'Iván', lastName: 'López', nested: [a: 1, b: 2]]).save(flush: true)
-        new TestMapJsonb(data: [name: 'Alonso', lastName: 'Torres', nested: [a: 2, b: 3]]).save(flush: true)
-        new TestMapJsonb(data: [name: 'Iván', lastName: 'Pérez', nested: [a: 1, b: 5]]).save(flush: true)
+            new TestMapJsonb(data: [name: 'Iván', lastName: 'López', nested: [a: 1, b: 2]]).save(flush: true)
+            new TestMapJsonb(data: [name: 'Alonso', lastName: 'Torres', nested: [a: 2, b: 3]]).save(flush: true)
+            new TestMapJsonb(data: [name: 'Iván', lastName: 'Pérez', nested: [a: 1, b: 5]]).save(flush: true)
 
         when:
-        def result = pgJsonbTestSearchService.search('pgJson', 'data', '#>>', '{nested, b}', '>', value)
+            def result = pgJsonbTestSearchService.search('pgJson', 'data', '#>>', '{nested, b}', '>', value)
 
         then:
             result.size() == size
@@ -45,9 +45,9 @@ class PgJsonbPathsIntegrationSpec extends Specification {
 
         where:
             value || size
-                1 || 3 // There are 3 items with nested.b > 1
-                3 || 1
-                6 || 0
+            1     || 3 // There are 3 items with nested.b > 1
+            3     || 1
+            6     || 0
     }
 
 }
